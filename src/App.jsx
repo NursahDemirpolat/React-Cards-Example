89e79dfb9ba75bcb8dc45c8433c6bf16fb2b577a
NursahDemirpolat <nursahdmrplt@gmail.com>
2023-05-22 16:35:24 +0300

    Resimleri props olarak göndermek

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import './App.css';
 import { Course } from './Course';
+import Angular from './images/angular.jpg'
+import Bootstrap from './images/bootstrap5.png'
+import KompleWeb from './images/kompleweb.jpg'
+import Frontend from './images/ccsharp.png'
 
 function App() {
   return (
     <div className="App">
-      <Course title="Angular" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
-"/>
-      <Course title="Bootstrap 5" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
-"/>
-      <Course title="Komple Web" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
-"/>
-      <Course title="Frontend" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
-"/>
+      <Course 
+        image={Angular}
+        title="Angular" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
+        deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+      "/>
+      <Course 
+        image={Bootstrap}
+        title="Bootstrap 5" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
+        deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+      "/>
+      <Course 
+        image={KompleWeb}
+        title="Komple Web" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
+        deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+      "/>
+      <Course 
+        image={Frontend}
+        title="Frontend" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
+        deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+      "/>
     </div>
   );
 }
--- src/Course.jsx
+++ src/Course.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export const Course = ({ title , description }) => {
+export const Course = ({ image , title , description }) => {
   // console.log(props)
   // const title=props.title;
   // const description=props.description;
   // const {title,description} = props;
   return (
     <div>
+      <img src={image} alt="" />
       <div>{title}</div>
       <div>{description}</div>
     </div>
