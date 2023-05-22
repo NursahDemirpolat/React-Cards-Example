d262e9dcb0ff7fc6513d9be1a82592ca96e16af9
NursahDemirpolat <nursahdmrplt@gmail.com>
2023-05-22 17:06:27 +0300

    Bulma css ekleyerek projeyi bitirme

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import './App.css';
 import { Course } from './Course';
 import Angular from './images/angular.jpg'
@@ -8,30 +9,51 @@ import Frontend from './images/ccsharp.png'
 function App() {
   return (
     <div className="App">
-      <Course 
-        image={Angular}
-        title="Angular" 
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
-        deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
-      "/>
-      <Course 
-        image={Bootstrap}
-        title="Bootstrap 5" 
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
-        deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
-      "/>
-      <Course 
-        image={KompleWeb}
-        title="Komple Web" 
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
-        deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
-      "/>
-      <Course 
-        image={Frontend}
-        title="Frontend" 
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
-        deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
-      "/>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">
+            Kurslarım
+          </p>
+        </div>
+      </section>
+      <div className="container">
+        <section className='section'>
+          <div className="columns">
+            <div className="column">
+              <Course 
+                image={Angular}
+                title="Angular" 
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
+                deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+              "/>
+            </div>
+            <div className='column'>
+              <Course 
+                image={Bootstrap}
+                title="Bootstrap 5" 
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
+                deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+              "/>
+            </div>
+            <div className='column'>
+              <Course 
+                image={KompleWeb}
+                title="Komple Web" 
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
+                deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+              "/>
+            </div>
+            <div className='column'>
+              <Course 
+                image={Frontend}
+                title="Frontend" 
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, 
+                deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+              "/>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
