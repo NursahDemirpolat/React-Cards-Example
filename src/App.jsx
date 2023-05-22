db5d94c6141c96d756beb48a35dba74b79c38ad3
NursahDemirpolat <nursahdmrplt@gmail.com>
2023-05-22 16:23:44 +0300

    Props kullanımı

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { Course } from './Course';
+
+function App() {
+  return (
+    <div className="App">
+      <Course title="Angular" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+"/>
+      <Course title="Bootstrap 5" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+"/>
+      <Course title="Komple Web" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+"/>
+      <Course title="Frontend" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates assumenda? Vero dignissimos, deleniti soluta non laboriosam nesciunt adipisci et unde voluptas reprehenderit, rem ab facilis consectetur reiciendis ipsum similique.
+"/>
+    </div>
+  );
+}
+
+export default App;
